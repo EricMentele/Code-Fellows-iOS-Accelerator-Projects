bd4b54915b9085af83db2ddef14e63fb1efd38c1
Eric Mentele <diviningsage@gmail.com>
2015-01-25 00:53:07 -0800

    Finished all assignment features.

--- Homework/Git It!/Git It!/GitMenuTableViewController.swift
+++ Homework/Git It!/Git It!/GitMenuTableViewController.swift
@@ -14,37 +14,37 @@ class GitMenuTableViewController: UITableViewController {
   override func viewDidLoad() {
     
     super.viewDidLoad()
-  }//view did load
+      }//view did load
   
   
   override func viewDidAppear(animated: Bool) {
     
     super.viewDidAppear(animated)
-    
     if GitCom.sharedGitCom.accessToken == nil {
       
-        GitCom.sharedGitCom.fetchAccessTokenCode()
-       }//ifGitCom
-      //set up object to be animated including starting point
-      let gitAlert = NSBundle.mainBundle().loadNibNamed("GitAlert", owner: self, options: nil).first as UIView
-      gitAlert.center = self.view.center
-      gitAlert.alpha = 0
-      gitAlert.transform = CGAffineTransformMakeScale(0.4, 0.4)
-      self.view.addSubview(gitAlert)
+      GitCom.sharedGitCom.fetchAccessTokenCode()
       
-      UIView.animateWithDuration(0.4, delay: 0.5, options: nil, animations: { () -> Void in
-        gitAlert.alpha = 1
-        gitAlert.transform =  CGAffineTransformMakeScale(1.0, 1.0)
-        }) { (finished) -> Void in
-          
-      }//animate
+//      let gitAlert = NSBundle.mainBundle().loadNibNamed("GitAlert", owner: self, options: nil).first as UIView
+//      gitAlert.center = self.view.center
+//      gitAlert.alpha = 0
+//      gitAlert.transform = CGAffineTransformMakeScale(0.4, 0.4)
+//      self.view.addSubview(gitAlert)
+//      
+//      UIView.animateWithDuration(0.4, delay: 0.5, options: nil, animations: { () -> Void in
+//        gitAlert.alpha = 1
+//        gitAlert.transform =  CGAffineTransformMakeScale(1.0, 1.0)
+//        }) { (finished) -> Void in
+  
+      //}//animate
+    }//ifGitCom
   }//viewDidAppear
   
+ 
   
-  @IBAction func gitAlertOk(sender: AnyObject) {
-    
-    let gitAlert = NSBundle.mainBundle().loadNibNamed("GitAlert", owner: self, options: nil).first as UIView
-    gitAlert.transform = CGAffineTransformMakeScale(0.0, 0.0)
-  }
+//  @IBAction func gitOK(sender: AnyObject) {
+//    
+//    
+//  }
+  
   
 }//GitMenu
